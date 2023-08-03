c07c1c94a38f63cbea14d74285afa662867d9022
Ilya Repkin <ilya.repkin@gmail.com>
2023-08-04 02:24:24 +0700

    Task 1 error - Kotlin.Unit

--- HW_Kotlin_2/src/main/kotlin/main.kt
+++ HW_Kotlin_2/src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
+val amount: Int = 99
+
 fun main () {
-    
+    println("платеж - " + amount)
+    println ("комиссия - " + comission(amount))
+}
+
+fun comission(amount: Int = 99) {
+    if (amount < 100) 35
+    else amount * 0.35
 }
