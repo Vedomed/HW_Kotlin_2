067edcf7297ed904e4ddaa00498616ca4731b67d
Ilya Repkin <ilya.repkin@gmail.com>
2023-08-04 15:25:51 +0700

    Исправление

--- HW_Kotlin_2/src/main/kotlin/main.kt
+++ HW_Kotlin_2/src/main/kotlin/main.kt
@@ -5,7 +5,5 @@ fun main () {
     println ("комиссия - " + comission(amount))
 }
 
-fun comission(amount: Int = 99) {
-    if (amount < 100) 35
-    else amount * 0.35
-}
+fun comission(amount: Int = 99) = if (amount < 100) 35 else amount * 0.35
+
